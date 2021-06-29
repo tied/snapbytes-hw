46370be75fd7e5acb74471e78c94f4074dc23cb3
acanozturk <acanozturk96@gmail.com>
2021-06-29 22:32:58 +0300

    updates

--- src/main/java/com/example/plugins/snapbytes/ShowLastComment.java
+++ src/main/java/com/example/plugins/snapbytes/ShowLastComment.java
@@ -1,27 +1,31 @@
 package com.example.plugins.snapbytes;
 
 import com.atlassian.jira.issue.Issue;
-import com.atlassian.jira.issue.changehistory.ChangeHistoryManager;
+import com.atlassian.jira.issue.changehistory.ChangeHistory;
+import com.atlassian.jira.issue.changehistory.DefaultChangeHistoryManager;
 import com.atlassian.jira.plugin.webfragment.contextproviders.AbstractJiraContextProvider;
 import com.atlassian.jira.plugin.webfragment.model.JiraHelper;
 import com.atlassian.jira.user.ApplicationUser;
+import lombok.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+@EqualsAndHashCode(callSuper = false)
+@AllArgsConstructor
 public class ShowLastComment extends AbstractJiraContextProvider {
 
-    private final ChangeHistoryManager changeHistoryManager;
-
-    public ShowLastComment(ChangeHistoryManager changeHistoryManager) {
-        this.changeHistoryManager = changeHistoryManager;
-    }
+    private final DefaultChangeHistoryManager changeHistoryManager;
 
     @Override
-    public Map<String, Object> getContextMap(final ApplicationUser applicationUser, final JiraHelper jiraHelper) {
-        final Map<String, Object> contextMap = new HashMap<>();
+    public Map<String, Object> getContextMap(final ApplicationUser user, final JiraHelper jiraHelper) {
         final Issue currentIssue = getCurrentIssue(jiraHelper);
-        final String showLastComment = "LAST COMMENT";
+        final List<ChangeHistory> changeHistory = getChangeHistory(currentIssue);
+        final ChangeHistory lastChange = getLastChange(changeHistory);
+        final String showLastComment = lastChange.getComment();
+
+        final Map<String, Object> contextMap = new HashMap<>();
 
         contextMap.put("showLastComment", showLastComment);
 
@@ -31,6 +35,16 @@ public class ShowLastComment extends AbstractJiraContextProvider {
     private Issue getCurrentIssue(final JiraHelper jiraHelper) {
 
         return (Issue) jiraHelper.getContextParams().get("issue");
+    }
+
+    private List<ChangeHistory> getChangeHistory(final Issue issue) {
+
+        return changeHistoryManager.getChangeHistories(issue);
+    }
+
+    private ChangeHistory getLastChange(final List<ChangeHistory> changeHistory) {
+
+        return changeHistory.get(changeHistory.size() - 1);
     }
 
 }
